d53d55201009ac6d4174a858441a17d75feac2ae
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-11-10 23:52:01 -0300

    filtrado por categorias

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
+const arrayComponentes = [];
+document.querySelectorAll("acordion-component").forEach(comp => arrayComponentes.push(comp));
 
-function buscarRespuesta(){
-    const busqueda = document.querySelector("#entrada")
-    const componente = document.querySelectorAll("acordion-component")
-    console.log(componente.entries(title))
-    console.log(componente.entries(title))
-    if (busqueda === componente.entries(title)){}
-    e.preventDefault();
+function filtrarBusqueda(e) {
+  e.preventDefault(); 
+  const entrada = document.querySelector("#entrada").value.trim().toLowerCase();
+  const coincidencia = arrayComponentes.find(comp => comp.getAttribute("title") === entrada);
+  
+  if (coincidencia) {
+    const insertarBusqueda => (coincidencia){
+        seccionBusqueda.innerHTML = "";
+
+        const HTML = `
+          <acordion-component title="${titulo}" class="${categoria}">
+                <span slot="contenido">${contenido}</span>
+            </acordion-component>
+        `;
+
+        seccionBusqueda.innerHTML += HTML;
+    };
+  } else {
+    return;
+  }
+}
+
+function filtrarCategoria(categoria) {
+  const seccionBusqueda = document.querySelector("#busqueda")
+
+  function iterarFiltrados(array){
+    seccionBusqueda.innerHTML = "";
+    array.forEach(comp => {
+        const categoria = comp.getAttribute("class"); 
+        const titulo = comp.getAttribute("title"); 
+        const contenido = comp.innerHTML; 
+    
+        // Generar un template por cada componente
+        const HTML = `
+          <acordion-component title="${titulo}" class="${categoria}">
+                <span slot="contenido">${contenido}</span>
+            </acordion-component>
+        `;
+
+        seccionBusqueda.innerHTML += HTML;
+    })
+  }
+
+  if (categoria === "precios") {
+    let arrayBusqueda = arrayComponentes.filter(comp => comp.classList.contains("precios"));
+    iterarFiltrados(arrayBusqueda);
+  } else if (categoria === "informacion") {
+    let arrayBusqueda = arrayComponentes.filter(comp => comp.classList.contains("informacion"));
+    iterarFiltrados(arrayBusqueda);
+  } else if (categoria === "resenas") {
+    let arrayBusqueda = arrayComponentes.filter(comp => comp.classList.contains("resenas"));
+    iterarFiltrados(arrayBusqueda);
+  } else {
+    alert("Le erraste, máquina!");
+    return; 
+  }
 }
