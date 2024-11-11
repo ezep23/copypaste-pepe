7dfac6f3352b65885eaac80762725a37bdfb8ecf
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-11-11 11:53:02 -0300

    terminado

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 const arrayComponentes = [];
 document.querySelectorAll("acordion-component").forEach(comp => arrayComponentes.push(comp));
 
-function filtrarBusqueda(e) {
-  e.preventDefault(); 
+const buscar = document.querySelector("#buscar")
+
+const filtrarBusqueda = (e) => {
+  e.preventDefault();
+  const seccionBusqueda = document.querySelector("#busqueda")
   const entrada = document.querySelector("#entrada").value.trim().toLowerCase();
-  const coincidencia = arrayComponentes.find(comp => comp.getAttribute("title") === entrada);
+  const coincidencia = arrayComponentes.find(comp => comp.getAttribute("title").toLowerCase() == entrada);
   
   if (coincidencia) {
-    function insertCoincidencia(coincidencia) {
-        seccionBusqueda.innerHTML = "";
+    seccionBusqueda.innerHTML = ""
 
-        const HTML = `
-          <acordion-component title="${coincidencia.getAttribute("title")}" class="${coincidencia.getAttribute("class")}">
-                <span slot="contenido">${comp.innerHTML}</span>
-            </acordion-component>
-        `;
+    let HTML = `
+    <div id="contenedor-busqueda">
+      <p>BÚSQUEDA</p>
+      <acordion-component title="${coincidencia.getAttribute("title")}" class="${coincidencia.getAttribute("class")}">
+        <span slot="contenido">${coincidencia.innerHTML}</span>
+      </acordion-component>
+    </div>
+    `;
 
-        seccionBusqueda.innerHTML += HTML;
-    };
-    insertCoincidencia;
+    seccionBusqueda.innerHTML += HTML;
   } else {
     return;
   }
 }
 
+buscar.addEventListener("click", filtrarBusqueda)
+
 function filtrarCategoria(categoria) {
-  const seccionBusqueda = document.querySelector("#busqueda")
+  
+  const seccionFiltrados = document.querySelector("#filtrados")
 
   function iterarFiltrados(array){
-    seccionBusqueda.innerHTML = "";
+    seccionFiltrados.innerHTML = "";
+
+    let textoFiltros = document.querySelector("#texto-filtro")
+    textoFiltros.innerText = "FILTROS"
+
     array.forEach(comp => {
         const categoria = comp.getAttribute("class"); 
         const titulo = comp.getAttribute("title"); 
         const contenido = comp.innerHTML; 
     
         // Generar un template por cada componente
-        const HTML = `
+        let HTML = `
           <acordion-component title="${titulo}" class="${categoria}">
                 <span slot="contenido">${contenido}</span>
-            </acordion-component>
+          </acordion-component>
         `;
 
-        seccionBusqueda.innerHTML += HTML;
+        seccionFiltrados.innerHTML += HTML;
     })
   }
 
